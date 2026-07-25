24904267325149dd7815fe52908e3673bed4cab6
Taufik Fadlurahman Fajari <taufikfadlurahmanfajari.dev@gmail.com>
2026-07-25 08:48:35 +0700

    initial setup

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Taufik Fadlurahman Fajari on 21/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
